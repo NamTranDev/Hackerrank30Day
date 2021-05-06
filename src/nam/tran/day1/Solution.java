6136e3b2cbc734fd9ed7aec5bd57d4bee17e8f11
NamTranDev <namtran09061992@gmail.com>
2021-05-06 15:32:19 +0700

    Solve challenge day 0 and day 1

---
+++ src/nam/tran/day1/Solution.java
@@ -0,0 +1,85 @@
+/*
+    Objective
+    Today, we're discussing data types. Check out the Tutorial tab for learning materials and an instructional video!
+
+    Task
+    Complete the code in the editor below. The variables , , and  are already declared and initialized for you. You must:
+
+    Declare  variables: one of type int, one of type double, and one of type String.
+    Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
+    Use the  operator to perform the following operations:
+    Print the sum of  plus your int variable on a new line.
+    Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+    Concatenate  with the string you read as input and print the result on a new line.
+    Note: If you are using a language that doesn't support using  for string concatenation (e.g.: C), you can just print one variable immediately
+    following the other on the same line. The string provided in your editor must be printed first, immediately followed by the string you read as input.
+
+    Input Format
+
+    The first line contains an integer that you must sum with .
+    The second line contains a double that you must sum with .
+    The third line contains a string that you must concatenate with .
+
+    Output Format
+
+    Print the sum of both integers on the first line, the sum of both doubles (scaled to  decimal place) on the second line, and then the two concatenated
+    strings on the third line.
+
+    Sample Input
+
+    12
+    4.0
+    is the best place to learn and practice coding!
+    Sample Output
+
+    16
+    8.0
+    HackerRank is the best place to learn and practice coding!
+    Explanation
+
+    When we sum the integers 4 and 12, we get the integer 16.
+    When we sum the floating-point numbers 4 and 4, we get 8.
+    When we concatenate HackerRank with is the best place to learn and practice coding!, we get HackerRank is the best place to learn and practice coding!.
+
+    You will not pass this challenge if you attempt to assign the Sample Case values to your variables instead of following the instructions above and reading
+    input from stdin.
+**/
+
+package nam.tran.day1;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+        StringBuilder result = new StringBuilder();
+
+        int count = 0;
+
+        while (count < 4){
+            count++;
+            if (scan.hasNextInt()) {
+                int resultI = scan.nextInt() + i;
+                result.append(resultI).append("\n");
+            } else if (scan.hasNextDouble()) {
+                double resultD = scan.nextDouble() + d;
+                result.append(resultD).append("\n");
+            } else {
+                String text = scan.nextLine();
+                if (text.isEmpty())
+                    continue;
+                result.append(s).append(text);
+            }
+        }
+
+        System.out.println(result);
+
+        scan.close();
+    }
+}
+
